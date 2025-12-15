06bc1092c0a38f52fe44214b2b00ebeb40bea5f4
Ravi S <gtasa7181@gmail.com>
2025-12-15 19:01:22 +0000

    Implement UCT selection and enhance simulation in TicTacToeNode
    Refactor TicTacToeNode to implement UCT-based selection and improve simulation logic.

--- MCTS C++/MCTS C++/MCTS/TicTacToeNode.cpp
+++ MCTS C++/MCTS C++/MCTS/TicTacToeNode.cpp
@@ -3,10 +3,9 @@
 #include "GameBoard.h"
 #include <AIBlackboard_Global.h>
 #include "AIRandom_Global.h"
-
+#include <cmath>
 
 // general functions
-
 BOARD_SQUARE_STATE getOppositeMove(BOARD_SQUARE_STATE state)
 {
 	// Flip to the other player
@@ -30,7 +29,6 @@ TicTacToeNode::TicTacToeNode()
 	wins = 0;
 	visits = 0;
 	activePlayer = BOARD_SQUARE_STATE::NONE;
-
 }
 
 TicTacToeNode::~TicTacToeNode()
@@ -44,58 +42,75 @@ TicTacToeNode::~TicTacToeNode()
 	}
 	branches.clear();
 	availableMoves.clear();
-
 }
-
 
 /*
 * Select - will perform the Select step of the MCTS algorithm
-*  will select down a path according to the selection policy until leaf is found
+* will select down a path according to the UCT selection policy until leaf is found
 */
 TicTacToeNode* TicTacToeNode::Select()
 {
-
 	if (availableMoves.size() > 0)
 	{
 		// still unexplored branches, explore
 		return this;
 	}
 
-	if (isEndState )
+	if (isEndState)
 	{
 		// leaf node, return and will be handled by Reasoner
 		return this;
 	}
 
-
-
-	// TASK TODO - replace below below code with UCT based selection
-
-
-	int randomBranch = rand() % branches.size();
-	return branches[randomBranch]->Select();
-
-
-
+	// **UCT-BASED SELECTION** - Choose best child using Upper Confidence Bound
+	double bestUCT = -999999.0;
+	TicTacToeNode* bestChild = nullptr;
+	
+	const double explorationParameter = sqrt(2.0);  // Standard UCT exploration parameter
+	
+	for (TicTacToeNode* child : branches)
+	{
+		if (child->getVisits() == 0)
+		{
+			// Unvisited nodes get maximum priority
+			return child->Select();
+		}
+		
+		// Calculate UCT value: exploitation + exploration
+		double exploitation = (double)child->getWins() / (double)child->getVisits();
+		double exploration = explorationParameter * sqrt(log((double)visits) / (double)child->getVisits());
+		double uctValue = exploitation + exploration;
+		
+		if (uctValue > bestUCT)
+		{
+			bestUCT = uctValue;
+			bestChild = child;
+		}
+	}
+	
+	if (bestChild != nullptr)
+	{
+		return bestChild->Select();
+	}
+	
+	return this;
 }
 
 /*
-* Expand  - will perform the Expansion step of the MCTS algorithm
+* Expand - will perform the Expansion step of the MCTS algorithm
 * will add a new node by randomly selecting from all possible moves
 */
 TicTacToeNode* TicTacToeNode::Expand()
 {
-
 	if (isEndState)
 	{
-		//std::cout << "Cannont expand - leaf " << std::endl;
 		return NULL; // if this is game end state, we cannot expand
 	}
 	else
 	{
-		// pick a random move fro, the unexplored ones
+		// pick a random move from the unexplored ones
 		int randomMove = rand() % availableMoves.size();
-		std::pair<int, int> move = availableMoves[randomMove];
+		int columnToPlay = availableMoves[randomMove];
 
 		// delete the selected move from vector
 		availableMoves.erase(availableMoves.begin() + randomMove);
@@ -111,85 +126,70 @@ TicTacToeNode* TicTacToeNode::Expand()
 		// create the new world state
 		GameState newWorldState;
 		newWorldState.gameBoard = this->worldState.gameBoard;
-		GameAction newAction(move.first, move.second, nextTurn);
+		GameAction newAction(columnToPlay, nextTurn);
 
 		// apply the move to the board and set the child's state
 		newWorldState.SetAndApplyAction(newAction);
 		childNode->setGameState(newWorldState);
 
-		// add the newly created node as a child of current node		
+		// add the newly created node as a child of current node 
 		branches.push_back(childNode);
 
-		// reutrn node, for simulation
+		// return node, for simulation
 		return childNode;
 	}
-
 }
-#/*
-* Simulate  - will perform the Simulate step of the MCTS algorithm
-* will play random moves until a end state is found, and set the simulationWinner
+
+/*
+* Simulate - will perform the Simulate step of the MCTS algorithm
+* will play random moves until an end state is found, and set the simulationWinner
 */
 void TicTacToeNode::Simulate(BOARD_SQUARE_STATE startingTurn)
 {
-
-
-
-	//TASK TODO - replace below code with the simulation functionality
-	// - ensure you set 'simulationWinner' in this function
-	// 
-	// # PSEUDOCODE
-	// 
-	//	Make copy of Game State
-	//	Set active player
-	//	WHILE game state NOT end of game :
-	//		Get all possible Moves
-	//		Randomly select one Move for active player
-	//		Apply Move to Game State
-	//		Set active player for next turn
-	//	
-	// Set simulationWinner to the simualation result
-
-
-
-	// Class/Functions of use:
-
-	//	Class			Function			Description
-	//	TicTacToeNode	GetGameState()		Will return the GameState for the current Node
-	//	TicTacToeNode	GetOppositeMove()	Will return the opposite marker than the one provided.E.g. if X is provided, O will be returned.
-	//	GameState		CheckWin()			If the game state is a winning state, will return BOARD_SQUARE_STATE of the winner, otherwise ‘NONE’
-	//	GameState		GetPossibleMoves()	Will return a vector of possible moves from the game state.Each move is a std::pair<int, int> representing <Row, Col>
-	//	GameAction		GameAction()		A new GameAction can be created using the GameAction constructor, with parameters Row, Col, Marker
-	//	AIRandom_Global	GetRandom01()		Will return a float between 0 and 1
-	//	AIRandom_Global	GetRandomValue()	Will return a value between 0 and the value provided
-
-
-
-
-	if (AIRandom_Global::getInstance()->GetRandom01() >= 0.5f)
+	// Make copy of Game State
+	GameState simulationState = worldState;
+	BOARD_SQUARE_STATE currentPlayer = startingTurn;
+	
+	// WHILE game state NOT end of game:
+	while (true)
 	{
-		simulationWinner = BOARD_SQUARE_STATE::CIRCLE;
+		// Check for win
+		BOARD_SQUARE_STATE winner = simulationState.CheckWin();
+		if (winner != BOARD_SQUARE_STATE::NONE)
+		{
+			simulationWinner = winner;
+			return;
+		}
+		
+		// Get all possible Moves
+		std::vector<int> possibleColumns = simulationState.GetPossibleMoves();
+		
+		// Check for draw (no moves left)
+		if (possibleColumns.size() == 0)
+		{
+			simulationWinner = BOARD_SQUARE_STATE::NONE;  // Draw
+			return;
+		}
+		
+		// Randomly select one Move for active player
+		int randomIndex = AIRandom_Global::getInstance()->GetRandomValue((int)possibleColumns.size());
+		int selectedColumn = possibleColumns[randomIndex];
+		
+		// Apply Move to Game State
+		GameAction simulatedAction(selectedColumn, currentPlayer);
+		simulationState.SetAndApplyAction(simulatedAction);
+		
+		// Set active player for next turn
+		currentPlayer = getOppositeMove(currentPlayer);
 	}
-	else
-	{
-		simulationWinner = BOARD_SQUARE_STATE::CROSS;
-	}
-
-
-
-
 }
 
-
-
 /*
-* CalcResult will determing if the AI won or lost, and trigger the back propagation
+* CalcResult will determining if the AI won or lost, and trigger the back propagation
 */
-
 void TicTacToeNode::CalcResult()
 {
-
 	BOARD_SQUARE_STATE _aiMarker = (BOARD_SQUARE_STATE)AIBlackboard_Global::getInstance()->GetValueInt("AIMarker");
-
 	if (simulationWinner == _aiMarker)
 	{
 		Backpropagate(1);
@@ -204,74 +204,67 @@ void TicTacToeNode::CalcResult()
 	}
 }
 
-
-
 /*
-* Backpropagate  - will perform the Backpropagate step of the MCTS algorithm
+* Backpropagate - will perform the Backpropagate step of the MCTS algorithm
 * the result will be returned back up the tree branches to the root node
 */
-
 void TicTacToeNode::Backpropagate(int result)
 {
-
-	//TASK TODO
-
-	// incerement visits
-	// increment wins depending on result (1 = AI Win, 0 = Draw, -1 = Opponent (Player) Win)
-
-	// if the node has a parent, call parent-> Backpropagate with the results
-	// the node will be NULL if there is no parment
-
-
-
-
+	// Increment visits
+	visits++;
+	
+	// Increment wins depending on result (1 = AI Win, 0 = Draw, -1 = Opponent Win)
+	// We store wins from the perspective of the AI, so flip result for opponent nodes
+	if (result == 1)
+	{
+		wins++;
+	}
+	else if (result == -1)
+	{
+		// Opponent win means this node led to a loss
+		// Don't increment wins
+	}
+	else
+	{
+		// Draw - give partial credit
+		wins += 0.5;
+	}
+	
+	// If the node has a parent, call parent->Backpropagate with the results
+	if (parent != nullptr)
+	{
+		parent->Backpropagate(result);
+	}
 }
 
-
-
-
 // status tracking and updates
-
-
-
 /**
-*  setGameState will set the game state for the node, as well as calling generatePossibleMoves to update status
+* setGameState will set the game state for the node, as well as calling generatePossibleMoves to update status
 */
-
 void TicTacToeNode::setGameState(GameState newWorldState)
 {
 	worldState = newWorldState;
 	generatePossibleMoves();
 }
 
-
 /**
-*  generatePossibleMoves will calculate the possible mvoes from this node and set the isEndState variable appropriately
-*  should only be called once when node's game state is defined
+* generatePossibleMoves will calculate the possible moves from this node and set the isEndState variable appropriately
+* should only be called once when node's game state is defined
 */
 void TicTacToeNode::generatePossibleMoves()
 {
-
 	availableMoves = worldState.GetPossibleMoves();
-
 	if (availableMoves.size() == 0)
 		isEndState = true;
-
 	if (worldState.CheckWin() != BOARD_SQUARE_STATE::NONE)
 		isEndState = true;
 }
 
-
-
-
-
 /*
 * FindHighestRankingChild will determine which of this node's children has the highest metric
 */
-
 TicTacToeNode* TicTacToeNode::FindHighestRankingChild()
 {
-
 	if (branches.size() == 0)
 		return NULL;
 
@@ -280,9 +273,7 @@ TicTacToeNode* TicTacToeNode::FindHighestRankingChild()
 
 	for (int i = 0; i < branches.size(); i++)
 	{
-
 		float nodeWinRate = ((float)branches[i]->getWins()) / branches[i]->getVisits();
-
 		if (nodeWinRate > maxWins)
 		{
 			maxIndex = i;
@@ -293,10 +284,9 @@ TicTacToeNode* TicTacToeNode::FindHighestRankingChild()
 	return branches[maxIndex];
 }
 
-#/*
+/*
 * resetNode will return the node to its default values
 */
-
 void TicTacToeNode::resetNode()
 {
 	branches.clear();
