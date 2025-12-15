39cdc3d23ba117910575535646f895b16b3cbbec
Ravi S <gtasa7181@gmail.com>
2025-12-15 19:30:02 +0000

    Fix formatting in MakeMove function declaration

--- MCTS C++/MCTS C++/MCTS/GameController.cpp
+++ MCTS C++/MCTS C++/MCTS/GameController.cpp
@@ -561,7 +561,8 @@ bool GameController::isValidMove(int _row, int _col)
 /*
 * MakeMove will update the game board with the provided row/col. The move should be a valid one, checked by isValidMove(). 
 */
-void GameController::MakeMove(int _col)  // Connect 4 only needs column{{
+void GameController::MakeMove(int _col)  // Connect 4 only needs column{
+{
 	mainGameState.SetAndApplyAction(GameAction(_col, markerTurn));
 
     std::ostringstream  os;
