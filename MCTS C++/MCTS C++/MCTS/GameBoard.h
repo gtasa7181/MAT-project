3631091a7e5f2e16e47e83a08f84ca5c1235571b
Ravi S <gtasa7181@gmail.com>
2025-12-15 18:52:35 +0000

    Refactor GameBoard for Connect Four implementation

--- MCTS C++/MCTS C++/MCTS/GameBoard.h
+++ MCTS C++/MCTS C++/MCTS/GameBoard.h
@@ -1,46 +1,42 @@
 #pragma once
 
-/*
-GameBoard defines enums and structures for the Tic Tac Toe game
- - BOARD_SQUARE_STATE used for markers
- - GameBoard to represent a 3x3 game board
- - GameAction to represent an action taken by a player (i.e. place marker X in row 1, col 2)
 
+/*
+GameBoard defines enums and structures for the Connect Four game
+    - BOARD_SQUARE_STATE used for markers
+    - GameBoard to represent a 6x7 game board  
+    - GameAction to represent an action taken by a player (i.e. place marker in column)
 */
+
 
 // define enum's and structs used in the game
 
 // BOARD_SQUARE_STATE used for markers
 enum BOARD_SQUARE_STATE { NONE, CIRCLE, CROSS };
 
-// GameBoard to represent a 3x3 game board
+// GameBoard to represent a 6x7 game board
 struct GameBoard
 {
-	BOARD_SQUARE_STATE board[3][3]{ { BOARD_SQUARE_STATE::NONE } };
+	BOARD_SQUARE_STATE board[6][7];  // 6 rows x 7 columns for Connect 4
 };
 
 
-//GameAction to represent an action taken by a player (i.e. place marker X in row 1, col 2)
+//GameAction to represent an action taken by a player (i.e. place marker in column)
 struct GameAction
 {
-	int row, col;
+	int column;  // Only need column for Connect 4 (gravity handles row)
 	BOARD_SQUARE_STATE playerMove;
 
 public:
-	GameAction(int newRow, int newCol, BOARD_SQUARE_STATE move)
+	GameAction(int newCol, BOARD_SQUARE_STATE move)
 	{
-		row = newRow;
-		col = newCol;
+		column = newCol;
 		playerMove = move;
 	}
 
 	GameAction()
 	{
 		playerMove = BOARD_SQUARE_STATE::NONE;
-		row = 0;
-		col = 0;
+		column = -1;
 	}
-
-
 };
-
